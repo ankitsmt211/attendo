e2c5cd2e385c100e177727690c991f49e2ec1521
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-02-13 15:25:52 +0530

    Delete SecurityConstants.java
    sensitive info

--- src/main/java/com/springsecurity/attendance/config/SecurityConstants.java
+++
@@ -1,7 +0,0 @@
-package com.springsecurity.attendance.config;
-
-public class SecurityConstants {
-    public static final String JWT_SECRET="randkflleoaiefej23343flnoi33oinfaldfnei3lnalfrandkflleoaiefej23343flnoi33oinfaldfnei3lnalf";
-    public static final long JWT_EXPIRATION=700000;
-
-}
