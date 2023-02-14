260e76a099e35ca2350dc03bcc31d0854a12a605
alphaBEE <ankitsmt211@gmail.com>
2023-02-14 16:03:13 +0530

    adding SecurityConstants file

---
+++ src/main/java/com/springsecurity/attendance/config/SecurityConstants.java
@@ -0,0 +1,6 @@
+package com.springsecurity.attendance.config;
+
+public class SecurityConstants {
+    public static final String JWT_SECRET="secret-512-bit-key";
+    public static final Long JWT_EXPIRATION=7000000L;
+}
