1c3043c6160a0c0264ac27f5eb8c1337ff400d9d
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-12-03 20:59:58 +0530

    remove unused import

--- src/main/java/com/springsecurity/attendance/controller/AuthenticationController.java
+++ src/main/java/com/springsecurity/attendance/controller/AuthenticationController.java
@@ -11,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
