91859693de35bda77fc6e97dd5bd6141ab9af6d5
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-12-03 20:57:16 +0530

    refactoring custom response as a class

--- src/main/java/com/springsecurity/attendance/response/CustomResponse.java
+++ src/main/java/com/springsecurity/attendance/response/CustomResponse.java
@@ -2,8 +2,41 @@ package com.springsecurity.attendance.response;
 
 import org.springframework.http.HttpStatus;
 
-public record CustomResponse(HttpStatus status, String message) {
+public class CustomResponse {
+  private HttpStatus status;
+  private String message;
+  private String payload;
+  public CustomResponse(HttpStatus status,String message){
+    this.status=status;
+    this.message=message;
+  }
+
   public CustomResponse(HttpStatus status,String message,String payload){
     this(status,message);
+    this.payload=payload;
+  }
+
+  public HttpStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(HttpStatus status) {
+    this.status = status;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public String getPayload() {
+    return payload;
+  }
+
+  public void setPayload(String payload) {
+    this.payload = payload;
   }
 }
