1612421137bb6ba5b488c246be23bea5c976cad1
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-12-03 20:58:56 +0530

    add login component

--- frontend/attendance-manager/src/Components/Auth/Login.jsx
+++ frontend/attendance-manager/src/Components/Auth/Login.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import './auth.css'
 
 const Login = () => {
+
+  const [email,setEmail] = useState()
+  const [password,setPassword]=useState()
+  const navigate = useNavigate()
+
+  const handleLogin = async(e)=>{
+    e.preventDefault()
+
+    //change url for different endpoint
+    const url = `http://localhost:8080/api/v1/auth/login`
+
+    const user = {email,password}
+
+    const response = await fetch(url,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json",
+      },
+      body:JSON.stringify(user)
+    })
+
+    const responseData = await response.json()
+
+    if(responseData.status){
+      if(responseData.status=="OK"){
+        const token = responseData.payload
+        alert(token)
+        navigate("/")
+      }
+      if(responseData.status=="NOT_FOUND"){
+        alert(responseData.message)
+      }
+      else{
+        alert("something wrong")
+      }
+    }
+  }
+
   return (
-    <div>Login</div>
+    <div className='login-main-container'>
+    <h1>Logo</h1>
+    <p>Login</p>
+    <div className='login-container'>
+        <form method="post">
+            <input onChange={(e)=>setEmail(e.target.value)} type="email" name="mailid" id="mailid" placeholder='Email id' required={true}/>
+            <input  onChange={(e)=>setPassword(e.target.value)} type="password" name="password" id="password" placeholder='Password' required={true}/>
+            <button className='signup-btn' onClick={handleLogin}>Login</button>
+        </form>
+         <Link className='register-link' to="/register">Click here to register</Link>
+
+    </div>
+
+</div>
   )
 }
 
