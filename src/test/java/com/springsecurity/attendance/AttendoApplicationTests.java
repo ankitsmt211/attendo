ab367c8201d021f0360c5ea8e432cf333b0ca44e
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-12-10 13:34:02 +0530

    change runner class name

--- src/test/java/com/springsecurity/attendance/AttendoApplicationTests.java
+++ src/test/java/com/springsecurity/attendance/AttendoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TokensApplicationTests {
+class AttendoApplicationTests {
 
 	@Test
 	void contextLoads() {
