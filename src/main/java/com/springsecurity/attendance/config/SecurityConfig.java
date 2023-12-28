dee457f46b73a8ceaa5aaad865eeeb759c8a10aa
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2023-12-28 07:05:43 +0530

    remove jwtFilter and add config for oauth

--- src/main/java/com/springsecurity/attendance/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/attendance/config/SecurityConfig.java
@@ -4,44 +4,32 @@ import com.springsecurity.attendance.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Autowired
-    private final JwtAuthFilter jwtAuthFilter;
-
-    @Autowired
     private final UserEntityRepository userEntityRepository;
 
-    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserEntityRepository userEntityRepository) {
-        this.jwtAuthFilter = jwtAuthFilter;
+    public SecurityConfig( UserEntityRepository userEntityRepository) {
         this.userEntityRepository = userEntityRepository;
     }
-
 
     //oauth2Login adds oauth filter in the filter chain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.oauth2Login();
 
-        http.
-                csrf()
-                .disable()
+// another way to add client registration object
+//        http.oauth2Login(c -> {
+//            c.clientRegistrationRepository(clientRepository());
+//        });
+
+        http
                 .authorizeHttpRequests()
                 .anyRequest()
                 .authenticated();
@@ -50,27 +38,30 @@ public class SecurityConfig {
     }
 
     //keeps record of client registration object
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository(){
-        ClientRegistration clientRegistration = clientRegistration();
-
-        return new InMemoryClientRegistrationRepository(clientRegistration);
-    }
+//    @Bean
+//    public ClientRegistrationRepository clientRegistrationRepository(){
+//        ClientRegistration clientRegistration = clientRegistration();
+//
+//        return new InMemoryClientRegistrationRepository(clientRegistration);
+//    }
 
 
     //exposes auth manager to rest of application
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
-    return authenticationConfiguration.getAuthenticationManager();
-    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+//    return authenticationConfiguration.getAuthenticationManager();
+//    }
 
     // CommonOAuthProvider can be used for common auth providers with minimal configuration
-    public ClientRegistration clientRegistration(){
-        return CommonOAuth2Provider
-                .GITHUB
-                .getBuilder("github")
-                .clientId("CLIENT_ID")
-                .clientSecret("CLIENT_SECRET")
-                .build();
-    }
+//    public ClientRegistration clientRegistration(){
+//        return CommonOAuth2Provider
+//                .GITHUB
+//                .getBuilder("github")
+//                .clientId("CLIENT_ID")
+//                .clientSecret("CLIENT_SECRET")
+//                .build();
+//    }
+
+    //finally it can also be configured in properties file, so no need for registration object or repository bean
+    //oauth filter is required
 }
