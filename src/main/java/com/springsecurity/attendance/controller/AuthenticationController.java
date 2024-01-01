fcabec17906fd9028e438f17e3c2ff389cef1ddb
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-01-01 09:05:38 +0530

    CORS support

--- src/main/java/com/springsecurity/attendance/controller/AuthenticationController.java
+++ src/main/java/com/springsecurity/attendance/controller/AuthenticationController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/auth")
+@CrossOrigin
 public class AuthenticationController {
     @Autowired
     private final JwtService jwtService;
