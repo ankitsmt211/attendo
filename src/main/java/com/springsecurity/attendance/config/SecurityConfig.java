c609181e34976ffe20fe1c058f094b98b52a4daf
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-01-18 23:35:51 +0530

    update config to allow options request

--- src/main/java/com/springsecurity/attendance/config/SecurityConfig.java
+++ src/main/java/com/springsecurity/attendance/config/SecurityConfig.java
@@ -4,15 +4,21 @@ import com.springsecurity.attendance.repository.UserEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -33,7 +39,7 @@ public class SecurityConfig {
         //cors need to enabled, followed by @CrossOrigin annotation on controller for
         //request with auth token
         http.cors().and()
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/auth/**").permitAll()
           .requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
@@ -57,5 +63,16 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
     return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Component
+    public static class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET","POST", "PUT", "DELETE", "OPTIONS", "HEAD");
+        }
+
     }
 }
