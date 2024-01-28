ea1161b624ffaf6afd3e69e5f5c64fac43c4bc80
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-01-28 15:14:54 +0530

    using auth context for redirection or rendering

--- frontend/attendance-manager/src/Components/Home/Home.jsx
+++ frontend/attendance-manager/src/Components/Home/Home.jsx
@@ -1,17 +1,21 @@
 import '../Home/Home.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Register from '../Auth/register';
 import Login from '../Auth/Login';
 import Dashboard from '../Dashboard/Dashboard';
+import { useContext } from 'react';
+import { authContext } from '../../App';
+
 export default function Home() {
+  const { isLoggedIn } = useContext(authContext);
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={isLoggedIn ? <Dashboard /> : <Navigate to={'/login'} />} />
+          <Route path="/home" element={isLoggedIn ? <Dashboard /> : <Navigate to={'/login'} />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
