cd7ec0426add1e34e050b9e24b218641fdb1a601
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-01-28 15:14:04 +0530

    wrapping root in auth context

--- frontend/attendance-manager/src/App.jsx
+++ frontend/attendance-manager/src/App.jsx
@@ -4,11 +4,17 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '../src/App.css';
+import { createContext, useState } from 'react';
+
+export const authContext = createContext();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem('token') != null);
   return (
     <>
-      <Home />
+      <authContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+        <Home />
+      </authContext.Provider>
     </>
   );
 }
